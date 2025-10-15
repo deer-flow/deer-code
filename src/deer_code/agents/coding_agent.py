2da213107a56278a1cfb1c5d0d17d06ac0600e24
Henry Li <henry1943@163.com>
2025-10-15 18:00:42 +0800

    refactor: move the `PROJECT_ROOT` param to create_coding_agent

--- src/deer_code/agents/coding_agent.py
+++ src/deer_code/agents/coding_agent.py
@@ -1,5 +1,6 @@
 from langgraph.prebuilt import create_react_agent
 
+from deer_code import project
 from deer_code.models import create_chat_model
 from deer_code.prompts.template import apply_prompt_template
 from deer_code.tools import bash_tool, text_editor_tool
@@ -9,7 +10,7 @@ def create_coding_agent():
     return create_react_agent(
         model=create_chat_model(),
         tools=[bash_tool, text_editor_tool],
-        prompt=apply_prompt_template("coding_agent"),
+        prompt=apply_prompt_template("coding_agent", PROJECT_ROOT=project.root_dir),
     )
 
 
--- src/deer_code/prompts/template.py
+++ src/deer_code/prompts/template.py
@@ -2,8 +2,6 @@ import os
 
 from jinja2 import Environment, FileSystemLoader
 
-from deer_code import project
-
 
 def apply_prompt_template(template: str, **kwargs) -> str:
     template_dir = os.path.join(os.path.dirname(__file__), "templates")
@@ -12,9 +10,4 @@ def apply_prompt_template(template: str, **kwargs) -> str:
     template = env.get_template(f"{template}.md")
     return template.render(
         **kwargs,
-        PROJECT_ROOT=(project.root_dir),
     )
-
-
-if __name__ == "__main__":
-    print(apply_prompt_template("coding_agent"))
