07249a291208e85ac49eed4096b4e10e6c7b21a9
Henry Li <henry1943@163.com>
2025-10-16 10:27:42 +0800

    fix: singleton

--- src/deer_code/tools/bash_tool.py
+++ src/deer_code/tools/bash_tool.py
@@ -3,7 +3,7 @@ from langchain.tools import tool
 from deer_code import project
 from deer_code.terminals import BashTerminal
 
-keep_alive_terminal = BashTerminal()
+keep_alive_terminal: BashTerminal | None = None
 
 
 @tool("bash", parse_docstring=True)
@@ -15,11 +15,12 @@ def bash_tool(command: str, reset_cwd: bool = False):
         reset_cwd: Whether to reset the current working directory to the project root directory.
     """
     global keep_alive_terminal
-    if reset_cwd:
-        if keep_alive_terminal:
-            keep_alive_terminal.close()
+    if keep_alive_terminal is None:
+        keep_alive_terminal = BashTerminal(project.root_dir)
+    elif reset_cwd:
+        keep_alive_terminal.close()
         keep_alive_terminal = BashTerminal(project.root_dir)
     return (
-        keep_alive_terminal.execute(command)
-        + "\n\nIMPORTANT: Next, update the TODO list using `todo_write` immediately."
+        f"```\n{keep_alive_terminal.execute(command)}\n```"
+        f"\n\nIMPORTANT: Next, update the TODO list using `todo_write` immediately."
     )
