e9449da073c4e50c934443af9a93433d2453bacc
Henry Li <henry1943@163.com>
2025-10-23 13:40:35 +0800

    feat: enhance todo write tool to report on unfinished tasks

--- src/deer_code/tools/todo/tool.py
+++ src/deer_code/tools/todo/tool.py
@@ -4,7 +4,7 @@ from langchain.messages import ToolMessage
 from langchain.tools import InjectedToolCallId, tool
 from langgraph.graph.state import Command
 
-from .types import TodoItem
+from .types import TodoItem, TodoStatus
 
 
 @tool("todo_write", parse_docstring=True)
@@ -17,12 +17,23 @@ def todo_write_tool(
         todos: A list of TodoItem objects.
     """
     # Do nothing, but save the latest to-dos to the state.
+
+    unfinished_todos = []
+    for todo in todos:
+        if todo.status != TodoStatus.completed and todo.status != TodoStatus.cancelled:
+            unfinished_todos.append(todo)
+
+    message = f"Successfully updated the TODO list with {len(todos)} items."
+    if len(unfinished_todos) > 0:
+        message += f" {len(unfinished_todos)} todo{' is' if len(unfinished_todos) == 1 else 's are'} not completed."
+    message += " All todos are completed."
+
     return Command(
         update={
             "todos": todos,
             "messages": [
                 ToolMessage(
-                    f"Successfully updated the TODO list with {len(todos)} items.",
+                    message,
                     tool_call_id=tool_call_id,
                 )
             ],
